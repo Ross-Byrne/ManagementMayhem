1bdff4baa83d3addba9eefe0f557b38401530283
Ross Byrne <ross.byrne@outlook.ie>
2014-10-28 15:18:17 +0000

    Adding some place holders.
    Still figuring out what I'm doing.

---
+++ src/GameFiles/Business.java
@@ -0,0 +1,7 @@
+package GameFiles;
+
+/* This class handles all the info about the player's business. */
+
+public class Business {
+
+}
--- src/GameFiles/Character.java
+++ src/GameFiles/Character.java
@@ -4,5 +4,17 @@ package GameFiles;
  * such as Name, traits, money, etc... */
 
 public class Character {
+	
+	String playerName;
+	int playerAge;
+	float playerMoney;
+	String[] playerTraits = {"", "", "", "", ""}; // string array to store 5 player traits
+	
+	Character()
+	{
+		playerName = "Player";
+		playerAge = 25;
+		playerMoney = 100000;
+	}
 
-}
+} // Class Character
