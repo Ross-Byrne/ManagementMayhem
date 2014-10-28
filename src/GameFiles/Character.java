a9d4202231c0dc89af457efe30b3d61cfec6dcab
Ross Byrne <ross.byrne@outlook.ie>
2014-10-28 14:41:34 +0000

    Just some setup work

---
+++ src/GameFiles/Character.java
@@ -0,0 +1,8 @@
+package GameFiles;
+
+/* This is the class that handles the characters details 
+ * such as Name, traits, money, etc... */
+
+public class Character {
+
+}
---
+++ src/GameFiles/MainClass.java
@@ -0,0 +1,24 @@
+package GameFiles;
+
+/* This is a game about managing your business.
+ * Keep you're business running at all costs. */
+
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+		displayStartMessage();
+		
+		
+		
+
+	} // main()
+	
+	public static void displayStartMessage()
+	{
+		System.out.println("Welcome to Management Mayhem!\n"
+				+ "As the sole owner and manager of your business,\n"
+				+ "it is your duty to keep it running at all costs.\n");
+	} // displayStartMessage()
+
+}
