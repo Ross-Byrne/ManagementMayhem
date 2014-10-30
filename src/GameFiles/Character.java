dacb243b43eeaae95973c1234a08712bfd9be8ad
Ross Byrne <ross.byrne@outlook.ie>
2014-10-30 10:32:14 +0000

    Working away

--- src/GameFiles/Character.java
+++ src/GameFiles/Character.java
@@ -5,16 +5,32 @@ package GameFiles;
 
 public class Character {
 	
+	// Member Variables
+	/*==============================================================================================================*/
 	String playerName;
 	int playerAge;
 	float playerMoney;
 	String[] playerTraits = {"", "", "", "", ""}; // string array to store 5 player traits
 	
+	// Constructors
+	/*==============================================================================================================*/
 	Character()
 	{
-		playerName = "Player";
+		setPlayerName("Player");
 		playerAge = 25;
 		playerMoney = 100000;
 	}
+	
+	// Methods 
+	/*==============================================================================================================*/
+	public void setPlayerName(String name)
+	{
+		playerName = name;
+	} // setPlayerName()
+	
+	public String getPlayerName()
+	{
+		return playerName;
+	} // getPlayerName()
 
 } // Class Character
